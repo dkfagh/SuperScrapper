84c606eddfe776f94265d3c2a38f8dd7b8fd746c
dkfagh <dkfagh@gmail.com>
2020-11-15 05:37:18 +0000

    #4.2

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("SuperScrapper")
 
 
 @app.route("/")
 def home():
-    return "Hello! Welcome to mi casa!"
-
-
-@app.route("/contact")
-def contact():
-    return "Contact me!"
+    return render_template("potato.html")
 
 
 app.run(host="0.0.0.0")
