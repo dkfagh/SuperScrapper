d7e952df4a544bb09941646a623608a91a7f2b01
dkfagh <dkfagh@gmail.com>
2020-11-15 05:51:22 +0000

    #4.3

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("SuperScrapper")
 
 
+# @ = decorator
+# 바로 아래에 있는 함수를 찾아서 실행
 @app.route("/")
 def home():
     return render_template("potato.html")
+
+
+@app.route("/report")
+def report():
+    # request에서 word라는 이름의 매개변수를 검색
+    word = request.args.get('word')
+    return render_template("report.html", searchingBy=word)
 
 
 app.run(host="0.0.0.0")
